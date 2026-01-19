4c04046b5a73c5ee48bc109e93a8a3ee1b252ac8
midhunmoh <164037658+midhunmoh@users.noreply.github.com>
2026-01-20 01:55:20 +0530

    urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('institutions.urls')),
-    path('api/', include('courses.urls')),
+     path('api/courses/', include('courses.urls')),
     path('api/auth/', include('authentication.urls')),
 ]
